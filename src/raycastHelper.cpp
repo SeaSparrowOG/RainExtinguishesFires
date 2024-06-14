d6090e149501a06a384b0484ff110d65b4331404
SeaSparrow <contactMeOnNexus@gmail.com>
2024-06-14 22:56:54 +0300

    Simplified raycast further

--- src/raycastHelper.cpp
+++ src/raycastHelper.cpp
@@ -7,9 +7,9 @@ namespace Raycast {
 		RE::NiPoint3 ray_start, ray_end;
 
 		ray_start = caster->data.location;
-		ray_start.z += 100.0f;
+		ray_start.z += 100.0;
 		ray_end = ray_start;
-		ray_end.z += 50000.0f;
+		ray_end.z += 50000;
 
 		pick_data.rayInput.from = ray_start * havokWorldScale;
 		pick_data.rayInput.to = ray_end * havokWorldScale;
@@ -20,12 +20,9 @@ namespace Raycast {
 			static_cast<uint32_t>(RE::COL_LAYER::kCharController);
 
 		caster->GetParentCell()->GetbhkWorld()->PickObject(pick_data);
-		RE::NiPoint3 hitpos;
+		RE::NiPoint3 hitpos = ray_end;
 		if (pick_data.rayOutput.HasHit()) {
 			hitpos = ray_start + (ray_end - ray_start) * pick_data.rayOutput.hitFraction;
-		}
-		else {
-			hitpos = ray_end;
 		}
 		return hitpos;
 	}
